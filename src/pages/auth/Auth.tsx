e875a36053c027f275fea503d3c838385043f7bc
Naman Shukla <namanshukla8707@gmail.com>
2025-12-22 23:57:32 +0530

    auth form in progress

--- src/pages/auth/Auth.tsx
+++ src/pages/auth/Auth.tsx
@@ -1,6 +1,5 @@
-import { Text } from "@/components/ui/CustomText";
 import "@/styles/scssStyles/authStyles/Auth.scss";
-import { AuthTile } from "./Auth.helper";
+import { AuthPageLeftPortion, AuthPageRightPortion } from "./Auth.helper";
 import { AuthPageStyles } from "@/styles/typescriptStyles/authStyles/AuthPage";
 import { ConvoHatIcon } from "@/assets/icons/auth/icon.convo-hat";
 import { BookIcon } from "@/assets/icons/auth/icon.book";
@@ -20,33 +19,10 @@ export const Auth = () => {
       iconDivStyle: AuthPageStyles.authStudentsTileIconStyle,
     },
   ];
-
   return (
     <div className="main-auth-box">
-      <div className="auth-left-box">
-        <Text
-          children={"Join EduVerse"}
-          style={AuthPageStyles.authLeftBoxMainHeadingStyles}
-        />
-        <Text
-          style={AuthPageStyles.authLeftBoxMainDescriptionStyles}
-          children={
-            "Start your learning journey today with expert-led courses."
-          }
-        />
-        <div className="main-auth-tile-box">
-          {authTileList.map((item, key) => (
-            <AuthTile
-              key={key}
-              heading={item.heading}
-              description={item.description}
-              svgIcon={item.svgIcon}
-              iconDivStyle={item.iconDivStyle}
-            />
-          ))}
-        </div>
-      </div>
-      <div></div>
+      <AuthPageLeftPortion authTileList={authTileList} />
+      <AuthPageRightPortion/>
     </div>
   );
 };
